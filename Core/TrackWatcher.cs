fea107fe7f6c4233c1f468e64a54f03452313726
JangoBrick <mail@jangobrick.de>
2017-07-01 22:43:50 +0200

    Add TrackWatcher and use in MainWindow

---
+++ Core/TrackWatcher.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using WindowTitleWatcher;
+using WindowTitleWatcher.Util;
+
+namespace Spofyp.Core
+{
+    public class TrackWatcher
+    {
+        private RecurrentWatcher watcher;
+
+        public Track CurrentTrack
+        {
+            get;
+            private set;
+        }
+        public event EventHandler TrackChange;
+
+        public TrackWatcher()
+        {
+            watcher = new RecurrentWatcher(() => Windows.FindFirst(IsMatchingWindow));
+            Update(watcher.Title);
+            watcher.TitleChanged += Watcher_TitleChanged;
+        }
+
+        private void Watcher_TitleChanged(object sender, TitleEventArgs e)
+        {
+            Update(e.NewTitle);
+        }
+
+        private void Update(string title)
+        {
+            Track prev = CurrentTrack;
+            CurrentTrack = Parse(title);
+
+            if (CurrentTrack != prev)
+            {
+                TrackChange?.Invoke(this, EventArgs.Empty);
+            }
+        }
+
+        private Track Parse(string title)
+        {
+            if (title == null)
+            {
+                return null;
+            }
+
+            int index = title.IndexOf(" - ");
+            if (index < 0)
+            {
+                return null;
+            }
+            
+            string artist = title.Substring(0, index);
+            string name = title.Substring(index + 3);
+
+            return new Track(name, artist);
+        }
+
+        private bool IsMatchingWindow(WindowInfo window)
+        {
+            if (!window.IsVisible)
+            {
+                return false;
+            }
+
+            string name = window.ProcessName;
+            string nameHalf = name.Substring(name.Length / 2);
+
+            return Hash(name) == 1683649665141281877 && Hash(nameHalf) == 17050863026783743523;
+        }
+
+        private static ulong Hash(string s)
+        {
+            ulong p = 16777619;
+            ulong v = 2166136261;
+
+            for (int i = 0; i < s.Length; ++i)
+            {
+                v = (v ^ s[i]) * p;
+            }
+
+            return v;
+        }
+    }
+}
--- Gui/MainWindow.cs
+++ Gui/MainWindow.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.WindowsAPICodePack.Dialogs;
+using Spofyp.Core;
 using System;
 using System.Data;
 using System.Diagnostics;
@@ -9,6 +10,8 @@ namespace Spofyp.Gui
 {
     public partial class MainWindow : Form
     {
+        private TrackWatcher watcher;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -18,6 +21,11 @@ namespace Spofyp.Gui
 
         private void MainWindow_Load(object sender, EventArgs e)
         {
+            // init watcher
+            watcher = new TrackWatcher();
+            UpdateCurrentlyPlaying(watcher.CurrentTrack);
+            watcher.TrackChange += Watcher_TrackChange;
+
             // set dest dir value
             DestDir_Input.Text = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyMusic), "Spofyp");
 
@@ -30,6 +38,28 @@ namespace Spofyp.Gui
             }
             TracksSource.DataSource = tracksData;
             TracksGrid.Sort(StartedAt, System.ComponentModel.ListSortDirection.Descending);
+        }
+
+        private void Watcher_TrackChange(object sender, EventArgs e)
+        {
+            UpdateCurrentlyPlaying(watcher.CurrentTrack);
+        }
+
+        private void UpdateCurrentlyPlaying(Track track)
+        {
+            if (CurrentlyPlaying_Value.InvokeRequired)
+            {
+                Invoke(new Action<Track>(UpdateCurrentlyPlaying), new[] { track });
+                return;
+            }
+
+            if (track == null)
+            {
+                CurrentlyPlaying_Value.Text = "(none)";
+            } else
+            {
+                CurrentlyPlaying_Value.Text = "\"" + track.Title + "\" by " + track.Artist;
+            }
         }
 
         private void DestDir_Button_Choose_Click(object sender, EventArgs e)
