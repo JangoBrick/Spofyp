b2cd5ccda74de64e503407a61ffeea3ae2f12efd
JangoBrick <mail@jangobrick.de>
2017-07-01 22:10:41 +0200

    Add Track class

---
+++ Core/Track.cs
@@ -0,0 +1,29 @@
+﻿namespace Spofyp.Core
+{
+    public class Track
+    {
+        public readonly string Title, Artist;
+
+        public Track(string title, string artist)
+        {
+            Title = title;
+            Artist = artist;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            var t = (Track)obj;
+            return (Title == t.Title) && (Artist == t.Artist);
+        }
+
+        public override int GetHashCode()
+        {
+            return Title.GetHashCode() ^ Artist.GetHashCode();
+        }
+    }
+}
